45007686969a77f28f900a112a7465dad18573d2
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-23 15:56:34 +0200

    [UPD] Migrated Type table and seeded[BONUS 1]

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Schema;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +16,22 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Schema::disableForeignKeyConstraints();
+
+        //SVUOTO LA TABELLA PRIMA DI POPOLARLA PER NON ACCUMULARE TROPPI ELEMENTI
+        Type::truncate();
+
+        //ARRAY DI PARTENZA
+        $types = ['Frontend', 'Backend', 'AI', 'Data Analytics', 'Fullstack'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->title = $type;
+            $new_type->slug = Str::of($type)->slug();
+
+            $new_type->save();
+        }
+
+        // Schema::enableForeignKeyConstraints();
     }
 }
